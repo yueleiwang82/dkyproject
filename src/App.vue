4a19f65744d0aec4c19f5ddbebcee69077ca8851
yueleiwang82 <wangyuelei82@163.com>
2019-09-29 15:56:02 +0800

    增加图片

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,36 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view />
     <footer id="footer">footer area</footer>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
-}
+  name: "App"
+};
 </script>
 
 <style  scoped lang="less">
 /* 全局自定义的css样式 */
-@import './styles/index.css';
+@import "./styles/index.css";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  #footer{
-    width:100%;
-    height:60px;
-    line-height:60px;
-    background:blueviolet;
-    text-align:center;
-    font-size:20px;
-    color:#fff;
+  width: 100vw;
+  height: 100vh;
+  #footer {
+    width: 100%;
+    height: 60px;
+    line-height: 60px;
+    background: blueviolet;
+    text-align: center;
+    font-size: 20px;
+    color: #fff;
   }
 }
 </style>
