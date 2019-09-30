6bd4b988fb1c7834edddfdfbc1f164aedfc89bd0
yueleiwang82 <wangyuelei82@163.com>
2019-09-30 09:24:54 +0800

    整理页面结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 // 主页路由
-import index from '@/views/index'
+import index from '@/views/public/index'
 Vue.use(Router)
 
 export default new Router({
